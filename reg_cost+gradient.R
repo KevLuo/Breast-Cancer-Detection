02c43ac72cf0a9e751f5e1e00d5761960f1a317a
KevLuo <kevinluo@Kevin-MacBook-Pro.local>
2017-08-05 00:38:32 -0700

    Added initial regularization code for cost and gradients

---
+++ reg_cost+gradient.R
@@ -0,0 +1,23 @@
+
+
+#Regularized cost function. Returns cost and gradients. Provides all the tools necessary to perform optimization.
+cost = function(theta, X, y, lambda) {
+  #number of training examples
+  m = NROW(y)
+  #h is the hypothesis predictions. It is 569 x 1.
+  h = sigmoid(X %*% theta)
+  #compute regularization term
+  reg_term = (lambda/(2*m)) * sum( theta(2:end)^2 )
+  #Compute Cost
+  J = (1/m) * ( (-1 * t(y) ) %*% log(h) - t((1-y)) %*% log(1-h)) + reg_term 
+  
+  #initialize gradient as 32 x 1 (same size as theta)
+  grad = as.vector(rep(0, 32))
+  #compute gradient regularization term
+  grad_reg_term = (lambda/m) * theta(2:end)
+  #Compute gradient
+  grad[1] = (1/m) * t(X[, 1]) %*% (h - y)
+  grad[2:end] = ( (1/m) * t(X[, 2:end]) %*% (h-y) ) + grad_reg_term
+  
+  return(list("cost" = J, "gradient" = grad))
+}
